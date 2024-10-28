d1edb742a361733191b672393821f9ae7d39c29e
Furkan Çoban <139001749+fc63@users.noreply.github.com>
2024-10-28 15:16:53 +0000

    Add files via upload

---
+++ h/bs.h
@@ -0,0 +1,7 @@
+#ifndef BS_H
+#define BS_H
+
+int bs();
+void bubbleSort();
+
+#endif //BS_H
---
+++ h/hs.h
@@ -0,0 +1,8 @@
+#ifndef HS_H
+#define HS_H
+
+int hs();
+void heapSort();
+void heapify(int i);
+
+#endif //HS_H
