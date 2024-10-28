00abbe4f61109fd9f2ead2dfe1757b38d7f013f9
Furkan Çoban <139001749+fc63@users.noreply.github.com>
2024-10-28 15:14:15 +0000

    Create quickSort.c

---
+++ sort/quickSort.c
@@ -0,0 +1,36 @@
+#include "h/functions.h"
+
+static int arr[] = {10,7,8,9,1,5};
+
+int part(int low, int high) {
+    int i = low - 1;
+    int j = 0;
+
+    for (j = low; j <= high - 1; j++) {
+        if (arr[j] < arr[high]) {
+            i++;
+            swap( & arr[i], & arr[j]);
+        }
+    }
+    swap( & arr[i + 1], & arr[high]);
+    return i + 1;
+}
+void quickSort(int low, int high) {
+    if (low < high) {
+        int pi = part(low, high);
+
+        quickSort(low, pi - 1);
+        quickSort(pi + 1, high);
+    }
+}
+int qs() {
+    printf("quickSort \n\nGiven array is \n");
+    printArray(arr, ARSZ(arr));
+
+    quickSort(0, ARSZ(arr) - 1);
+
+    printf("\nSorted array is \n");
+    printArray(arr,ARSZ(arr));
+
+    return 0;
+}
