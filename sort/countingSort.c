728063d8fdc0a144dac8eb6cec8eeafc3c3473e5
Furkan Çoban <139001749+fc63@users.noreply.github.com>
2024-10-28 15:40:03 +0000

    remove min remove -1 add prefix

--- sort/countingSort.c
+++ sort/countingSort.c
@@ -4,18 +4,17 @@ static int arr[] = {4,3,12,1,5,5,3,9};
 
 void countingSort() {
 
-    int max = maxarr(arr, ARSZ(arr)), i = 0;
-    int min = minarr(arr, ARSZ(arr)), n_arr[ARSZ(arr)];
-    int * pos = array0(max + 1 - min);
+    int n_arr[ARSZ(arr)], max = maxarr(arr, ARSZ(arr)), i = 0;
+    int * pos = array0(max + 1);
 
     for (i = 0; i < ARSZ(arr); i++)
-        pos[arr[i] - min]++; 
+        pos[arr[i]]++; 
 
     for (i = 1; i <= max; i++)
         pos[i] += pos[i - 1]; 
 
     for (i = ARSZ(arr) - 1; i >= 0; i--) 
-        n_arr[pos[arr[i] - min]-- - 1] = arr[i]; 
+        n_arr[--pos[arr[i]]] = arr[i]; 
 
     free(pos);
 
