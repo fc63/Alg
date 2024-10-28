7e04c11694e31b74aafff9db0a2b80fe3fb19394
Furkan Çoban <139001749+fc63@users.noreply.github.com>
2024-10-28 15:07:36 +0000

    Add files via upload

---
+++ main.c
@@ -0,0 +1,31 @@
+#include "h/functions.h"
+
+int main() {
+    int i = 0, j = 0;
+
+    printf("1. Sort\n2. Btr\n\nYour choice:");
+    scanf("%d",&i);
+    switch(i) {
+        case 1:
+        printf("1. Bubble Sort\n2. Insertion Sort\n3. Merge Sort\n4. Quick Sort\n5. Heap Sort\n6. Counting Sort\n\nYour choice:");
+        scanf("%d",&j);
+        printf("\n");
+        switch(j) {
+            case 1:
+                return bs();
+            case 2:
+                return is();
+            case 3:
+                return ms();
+            case 4:
+                return qs();
+            case 5:
+                return hs();
+            case 6:
+                return cs();
+        }
+        case 2:
+            return bt();
+    }
+    return 0;
+}
