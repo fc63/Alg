9d3b20d82ed99d1ab12e8e98b0fa06f9e193a2d1
Furkan Çoban <139001749+fc63@users.noreply.github.com>
2024-10-28 15:13:26 +0000

    Create insertionSort.c

---
+++ sort/insertionSort.c
@@ -0,0 +1,28 @@
+#include "h/functions.h"
+
+static int arr[] = {12,11,13,5,6};
+
+void insertionSort()
+{
+    int i = 0, j = 0, key = 0;
+    for (i = 1; i < ARSZ(arr); i++) {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+int is() {
+    printf("insertionSort \n\nGiven array is \n");
+    printArray(arr, ARSZ(arr));
+
+    insertionSort();
+
+    printf("\nSorted array is \n");
+    printArray(arr,ARSZ(arr));
+    return 0;
+}
