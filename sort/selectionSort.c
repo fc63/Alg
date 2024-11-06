60e10d4afe324733f861ae02e02785a09508a92a
fc63 <furkan@coban.email>
2024-11-06 09:31:39 +0300

    parantez vedası

--- sort/selectionSort.c
+++ sort/selectionSort.c
@@ -5,11 +5,10 @@ static int arr[] = {64,34,25,12,22,11,90};
 void selectionSort() {
     for (int i = 0; i < SIZE(arr) - 1; i++) {
         int min_idx = i;
-        for (int j = i + 1; j < SIZE(arr); j++) {
-            if (arr[j] < arr[min_idx]) {
+        for (int j = i + 1; j < SIZE(arr); j++)
+            if (arr[j] < arr[min_idx])
                 min_idx = j;
-            }
-        }
+
         swap(& arr[i], & arr[min_idx]);
     }
 }
