aaefa628a3bd9bff359344d4e716bcb6690b848b
fc63 <furkan@coban.email>
2024-11-10 13:56:27 +0300

    bruteforce

--- BruteForce/bruteForcePoly.c
+++ BruteForce/bruteForcePoly.c
@@ -2,11 +2,15 @@
 
 void bruteForcePoly(int P[], int n, int x) {
     int result = 0;
-    int power = 1;
 
     for (int i = 0; i <= n; i++) {
+        int power = 1;
+
+        for (int j = 0; j < i; j++)
+            power *= x;
+
+
         result += P[i] * power;
-        power *= x;
     }
 
     printf("Polynomial value at x = %d is %d\n", x, result);
